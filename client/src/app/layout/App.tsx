6dce9fdb0dfa6eae3bb8028feec545631d29d95b
Ronit Shahi <ronit@getmessiah.io>
2023-10-20 09:50:33 +1030

    end of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import Catalog from "../../features/catalog/catalog";
 import { Container, CssBaseline, createTheme, ThemeProvider } from "@mui/material";
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -23,7 +24,7 @@ function App() {
     <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-      <Catalog />
+      <Outlet />
       </Container>
     </ThemeProvider>
   )
